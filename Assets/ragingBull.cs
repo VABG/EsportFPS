ed377500464880d4739c646c6eac17c444b2c4aa
VABG <VABGProductions@hotmail.com>
2021-03-01 22:24:06 +0100

    Some minor updates and tests
    + Made gun material more complete
    ~ Modified animations a bit

--- Assets/ragingBull.cs
+++ Assets/ragingBull.cs
@@ -7,10 +7,19 @@ public class ragingBull : MonoBehaviour
     [SerializeField] GameObject shot;
     [SerializeField] Transform shotSpawn;
     [SerializeField] float shotLifeTime = 5.0f;
+    [SerializeField] Light shotLight;
+    [SerializeField] float shotLightLifeTime = .05f;
+    private float shotLightLifeCounter = 100;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void Update()
+    {
+        shotLightLifeCounter += Time.deltaTime;
+        if (shotLightLifeCounter > shotLightLifeTime) shotLight.enabled = false;
     }
 
 
@@ -19,5 +28,7 @@ public class ragingBull : MonoBehaviour
     {
         GameObject g = Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
         Destroy(g, shotLifeTime);
+        shotLight.enabled = true;
+        shotLightLifeCounter = 0;
     }
 }

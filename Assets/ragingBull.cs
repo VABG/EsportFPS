90570566f8074db3ee6364ecd83608ef35b2ab6d
VABG <VABGProductions@hotmail.com>
2021-03-09 11:39:26 +0100

    Lektionsinnehåll

--- Assets/ragingBull.cs
+++ Assets/ragingBull.cs
@@ -6,20 +6,25 @@ public class ragingBull : MonoBehaviour
 {
     [SerializeField] GameObject shot;
     [SerializeField] Transform shotSpawn;
+    [SerializeField] float bulletPower = 100;
     [SerializeField] float shotLifeTime = 5.0f;
     [SerializeField] Light shotLight;
     [SerializeField] float shotLightLifeTime = .05f;
     [SerializeField] List<GameObject> bulletsVisual;
+    [SerializeField] ParticleSystem shotPFX;
+
     private float shotLightLifeCounter = 100;
     public bool SlowMoReload = false;
     Animator anim;
     const int ammoMax = 6;
     int ammo = 6;
+    private Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        cam = FindObjectOfType<Camera>();
     }
 
     private void Update()
@@ -75,8 +80,21 @@ public class ragingBull : MonoBehaviour
 
     public void ShootBullet()
     {
-        GameObject g = Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-        Destroy(g, shotLifeTime);
+        Ray r = new Ray(cam.transform.position, cam.transform.forward);
+        RaycastHit rHit;
+
+        Physics.Raycast(r, out rHit);
+
+        Destroy(Instantiate(shotPFX, shotSpawn.position, shotSpawn.rotation).gameObject, .2f);
+
+        if (rHit.rigidbody != null)
+        {
+            rHit.rigidbody.AddForceAtPosition(r.direction * bulletPower, rHit.point);
+        }
+
+        //GameObject g = Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        //Destroy(g, shotLifeTime);
+
         shotLight.enabled = true;
         shotLightLifeCounter = 0;
     }
@@ -87,7 +105,7 @@ public class ragingBull : MonoBehaviour
         if (SlowMoReload) Time.timeScale = 1.0f;
         foreach (GameObject g in bulletsVisual)
         {
-                g.SetActive(true);            
+            g.SetActive(true);
         }
     }
 
